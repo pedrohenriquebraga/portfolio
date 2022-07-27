e050e9337e9438eb76d98cb6c46bcbcc81c5644d
pedrohenriquebraga <gamesantos7@gmail.com>
2022-07-27 15:54:42 -0300

    Add slider in project

--- src/components/Index/Projects/index.tsx
+++ src/components/Index/Projects/index.tsx
@@ -1,15 +1,117 @@
-import React from "react";
-import { Container } from "./styles";
+import React, { useMemo, useState } from "react";
+import Image from "next/image";
+import { Container, ProjectsSliderImagesContainer } from "./styles";
+import { FiArrowLeft, FiArrowRight } from "react-icons/fi";
 
 const Projects: React.FC = () => {
+  const [currentSlide, setCurrentSlide] = useState(0);
+  const slides = useMemo(
+    () => [
+      {
+        name: "Ecoleta",
+        image_url: "/ecoleta.webp",
+        github_url: "https://github.com/pedrohenriquebraga/nlw1",
+      },
+      {
+        name: "Clima Web",
+        image_url: "/clima-web.webp",
+        github_url: "https://github.com/pedrohenriquebraga/clima-web",
+      },
+      {
+        name: "Plant Manager",
+        image_url: "/plant-manager.webp",
+        github_url: "https://github.com/pedrohenriquebraga/nlw5",
+      },
+      {
+        name: "Saturn Chat Website",
+        image_url: "/saturn-chat-website.webp",
+        github_url: "https://github.com/flow-apps/saturn-chat-web",
+      },
+      {
+        name: "Green Force",
+        image_url: "/green-force.webp",
+        github_url: "https://github.com/pedrohenriquebraga/green-force",
+      },
+      {
+        name: "Happy",
+        image_url: "/happy.webp",
+        github_url: "https://github.com/pedrohenriquebraga/nlw3",
+      },
+      {
+        name: "Saturn Chat App",
+        image_url: "/saturn-chat-app.webp",
+        github_url: "https://github.com/flow-apps/saturn-chat-mobile",
+      },
+    ],
+    []
+  );
+
+  const handleSlideChange = (direction: "prev" | "next") => {
+    if (direction === "prev" && currentSlide === 0) {
+      return setCurrentSlide(slides.length - 1);
+    }
+
+    if (direction === "prev" && currentSlide > 0) {
+      return setCurrentSlide(currentSlide - 1);
+    }
+
+    if (direction === "next" && currentSlide === slides.length - 1) {
+      return setCurrentSlide(0);
+    }
+
+    if (direction === "next" && currentSlide < slides.length - 1) {
+      return setCurrentSlide(currentSlide + 1);
+    }
+  };
+
   return (
     <Container id="my-projects">
       <div id="my-projects-container">
         <h2>My projects</h2>
         <p>
-          This is all my projects designed and developed by me. All images
-          let&apos;s you to repository in my Github
+          These are all my projects designed and developed by me. all images
+          will take you to the repository on my Github. All projects here are
+          personal. I&apos;ve never done a professional project, but I&apos;m
+          looking for new Job opportunities. For more projects{" "}
+          <a
+            href="https://github.com/pedrohenriquebraga"
+            target="_blank"
+            rel="noopener noreferrer"
+          >
+            visit my Github
+          </a>
+          .
         </p>
+        <ProjectsSliderImagesContainer>
+          <div id="slider-container">
+            <div id="slider-controller-container">
+              <div
+                className="slider-controller prev"
+                onClick={() => handleSlideChange("prev")}
+              >
+                <FiArrowLeft />
+              </div>
+              <div
+                className="slider-controller next"
+                onClick={() => handleSlideChange("next")}
+              >
+                <FiArrowRight />
+              </div>
+            </div>
+            <div id="slider-main-image">
+              <a
+                href={slides[currentSlide].github_url}
+                target="_blank"
+                rel="external noreferrer"
+              >
+                <img
+                  src={slides[currentSlide].image_url}
+                  alt={slides[currentSlide].name}
+                />
+              </a>
+            </div>
+          </div>
+        </ProjectsSliderImagesContainer>
       </div>
     </Container>
   );
--- src/components/Index/Projects/styles.ts
+++ src/components/Index/Projects/styles.ts
@@ -2,6 +2,8 @@ import styled from 'styled-components';
 
 export const Container = styled.section`
   display: flex;
+  align-items: center;
+  justify-content: center;
   flex: 1;
   padding: 4rem 8rem;
   background-color: #5863d8;
@@ -16,6 +18,71 @@ export const Container = styled.section`
       font-size: 1.7rem;
       margin-bottom: 2rem;
       color: #fcfcfd;
+
+      a {
+        color: #fcfcfd;
+        text-decoration: underline;
+        &:hover {
+          color: #dedede;
+          transition: 500ms;
+        }
+      }
+    }
+  }
+`;
+
+export const ProjectsSliderImagesContainer = styled.div`
+  position: relative;
+  display: flex;
+  flex: 1;
+  width: 100%;
+  align-items: center;
+  justify-content: center;
+
+  #slider-controller-container {
+    position: absolute;
+    
+    display: flex;
+    width: 100%;
+    flex: 1;
+
+    justify-content: space-between;
+    align-items: center;
+
+    top: 50%;
+
+    .slider-controller {
+      cursor: pointer;
+      display: flex;
+      align-items: center;
+      justify-content: center;
+      font-size: 20px;
+      background: #00000055;
+      color: #fff;
+      border-radius: 50%;
+
+      width: 5rem;
+      height: 5rem;
+
+      &:hover {
+        background: #00000077;
+        transition: 500ms;
+      }
+    }
+  }
+
+  #slider-main-image {
+    display: flex;
+    flex: 1;
+    width: 100%;
+    user-select: none;
+
+    cursor: pointer;
+
+    img {
+      width: 100%;
+      object-fit: cover;
+      border-radius: 5px;
     }
   }
 `;
