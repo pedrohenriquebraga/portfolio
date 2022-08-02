4b25bf073bd3ff467ae4d1610061f9da2c42748a
pedrohenriquebraga <gamesantos7@gmail.com>
2022-08-02 18:41:33 -0300

    Fix google analytics

--- pages/_document.tsx
+++ pages/_document.tsx
@@ -49,15 +49,17 @@ export default class MyDocument extends Document {
                 async
                 src={`https://www.googletagmanager.com/gtag/js?id=${process.env.NEXT_PUBLIC_GA_MEASUREMENT_ID}`}
               />
-              <script>
-                {`
-                window.dataLayer = window.dataLayer || [];
-                function gtag(){dataLayer.push(arguments);}
-                gtag('js', new Date());
-    
-                gtag('config', '${process.env.NEXT_PUBLIC_GA_MEASUREMENT_ID}');
-            `}
-              </script>
+              <script
+                dangerouslySetInnerHTML={{
+                  __html: `
+                    window.dataLayer = window.dataLayer || [];
+                    function gtag(){dataLayer.push(arguments);}
+                    gtag('js', new Date());
+        
+                    gtag('config', '${process.env.NEXT_PUBLIC_GA_MEASUREMENT_ID}');
+                  `,
+                }}
+              ></script>
             </>
           )}
 
