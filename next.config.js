1a9751f2cf94791d3c552dc1b2c6dd33f6a5911a
pedrohenriquebraga <gamesantos7@gmail.com>
2022-08-02 12:45:06 -0300

    non www redirect resolved

--- next.config.js
+++ next.config.js
@@ -9,14 +9,6 @@ const nextConfig = {
       pure: true,
     },
   },
-  redirects: async () => [
-    {
-      source: '/:path*',
-      has: [{ type: 'host', value: `www.${process.env.NEXT_PUBLIC_DOMAIN_URL}` }],
-      destination: `${process.env.NEXT_PUBLIC_DOMAIN_URL}:path*`,
-      permanent: true
-    }
-  ]
 }
 
 module.exports = nextConfig
