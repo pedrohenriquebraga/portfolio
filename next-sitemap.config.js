bdba9f0479b950bc431f8f1a3b6bc24c40770637
pedrohenriquebraga <gamesantos7@gmail.com>
2022-12-30 19:54:08 -0300

    new sitemap

--- next-sitemap.config.js
+++ next-sitemap.config.js
@@ -5,7 +5,6 @@ module.exports = {
   siteUrl,
   exclude: ["/404", "/500", "/server-sitemap-server"],
   generateRobotsTxt: true,
-  generateIndexSitemap: false,
   robotsTxtOptions: {
     policies: [
       {
